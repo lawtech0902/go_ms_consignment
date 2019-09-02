151a14ba407e77e649feec28fc09453830e3c4d8
lawtech0902 <584563542@qq.com>
2019-09-02 16:24:13 +0800

    part 1: first service

---
+++ consignment_service/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	pb "go_projects/learngo/consignment_demo/consignment_service/proto/consignment"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"sync"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
+func main() {
+	repo := &Repository{}
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterShippingServiceServer(s, &service{repo})
+	log.Println("Running on port:", port)
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
